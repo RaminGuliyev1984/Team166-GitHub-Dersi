622dcdb6dd2b69402d850e884755022ce6a158d2
RaminGuliyev1984 <ramin@guliyev.com>
2023-04-26 19:16:26 +0300

    deneme

--- src/Day01/C01.java
+++ src/Day01/C01.java
@@ -2,3 +2,4 @@ package Day01;
 
 public class C01 {
 }
+//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
---
+++ src/Day01/deneme.java
@@ -0,0 +1,7 @@
+package Day01;
+
+public class deneme {
+    public static void main(String[] args) {
+        System.out.println("deneme");
+    }
+}
